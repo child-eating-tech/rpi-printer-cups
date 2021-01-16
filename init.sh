8e8f8754ca06c3d6ad4ace9fa006b6772fadce18
Leo Lingfeng Li <lilingfeng0408@outlook.com>
2021-01-16 19:01:00 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo apt-get install -f -y cups
 sudo usermod -a -G lpadmin pi
 ifconfig
-echo "Please open your browser and go to IP:631 for further config."
+echo "Please open your browser and go to http://localhost:631 for further config."
